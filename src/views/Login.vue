0f8ae976266d02e4457019f934ba16658853f56b
Moosa Akoodie <moosaakoodie3@gmail.com>
2024-08-12 01:00:42 +0200

    copied over the  header to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,58 @@
 <template>
-  <div class="login-container">
-    <form @submit.prevent="login">
-      <div class="form-group">
-        <label for="username">Username:</label>
-        <input type="text" v-model="username" id="username" required />
+  <div>
+    <!-- Header Component -->
+    <header class="header">
+      <div class="header-content">
+        <div class="brand">
+          <img src="../images/shop.png" alt="Brand Logo" class="brand-logo">
+          <h1 class="header-title">SwiftCart</h1>
+        </div>
+        <div class="header-right">
+          <h3 class="wishlist">
+            <a href="#" class="wishlist-btn">
+              <span class="wishlist-icon">♡</span>
+              <span class="wishlist-text">Wishlist</span>
+            </a>
+          </h3>
+          <div class="cart">
+            <a href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cart-icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"></path>
+              </svg>
+            </a>
+            <span class="cart-badge">{{ cartCount }}</span>
+          </div>
+          <!-- Conditional Login/Logout Button -->
+          <div class="login">
+            <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
+            <a href="#" v-else @click="logout">Logout</a>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="password">Password:</label>
-        <input :type="passwordFieldType" v-model="password" id="password" required />
-        <button type="button" @click="togglePasswordVisibility">
-          {{ passwordFieldType === 'password' ? 'Show' : 'Hide' }} Password
-        </button>
-      </div>
-      <button type="submit">Log In</button>
-    </form>
-    <div v-if="loading">Authenticating...</div>
-    <div v-if="error">{{ error }}</div>
+    </header>
+
+    <!-- Login Form -->
+    <div class="login-container">
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" v-model="username" id="username" required />
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input :type="passwordFieldType" v-model="password" id="password" required />
+          <button type="button" @click="togglePasswordVisibility">
+            {{ passwordFieldType === 'password' ? 'Show' : 'Hide' }} Password
+          </button>
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+      <div v-if="loading">Authenticating...</div>
+      <div v-if="error">{{ error }}</div>
+    </div>
   </div>
 </template>
+
 
 <script>
 export default {
